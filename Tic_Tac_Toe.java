b3da3ba205ad5ac3380088f52c948e13b59275ba
keerthana2624 <keerthanamummy2002@gmail.com>
2024-07-04 22:05:24 -1000

    fieds are initialized in a class

---
+++ Tic_Tac_Toe.java
@@ -0,0 +1,11 @@
+public class Tic_Tac_Toe {
+
+    private static final int size=3;
+    private static final char user1='x';
+    private static final int user2='o';
+
+
+    private char[] tictacboard;
+
+    
+}
