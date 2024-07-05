80f37421ee655afc7fbe1dc50a3ff83349c20a4c
keerthana2624 <keerthanamummy2002@gmail.com>
2024-07-04 23:50:20 -1000

    i have initialized board in the class

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -5,7 +5,39 @@ public class Tic_Tac_Toe {
     private static final int user2='o';
 
 
-    private char[] tictacboard;
+    private char[][] tictacboard;
+    private char currentplayer;
 
+    public Tic_Tac_Toe(){
+        initializeBoard();
+        currentplayer=user1;
+
+    }
+
+    private void initializeBoard(){
+        tictacboard = new char[size][size];
+        for(int row=0;row<size;row++){
+            for (int col=0;col<size;col++){
+                tictacboard[row][col]='-';
+            }
+        }
+    }
+
+    private void displayboard(){
+        for(int row=0;row<size;row++){
+            for (int col=0;col<size;col++){
+               System.out.print(tictacboard[row][col]);
+            }
+            System.out.println();
+        }
+    }
     
+    private void makeMove(){
+        
+    }
+
+    public static void main(String[] args) {
+        Tic_Tac_Toe tic=new Tic_Tac_Toe();
+        tic.displayboard();
+    }
 }
