4b7fd1a729a166faac9b093564d45dc5485e876d
keerthana2624 <keerthanamummy2002@gmail.com>
2024-07-05 15:47:51 -1000

    sucessfully implimented the tic tac toe

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 
-
 public class Tic_Tac_Toe {
 
     private static final int size=3;
     private static final char user1='x';
-    private static final int user2='o';
+    private static final char user2='o';
 
 
     private char[][] tictacboard;
@@ -29,13 +28,17 @@ public class Tic_Tac_Toe {
         for(int row=0;row<size;row++){
             for (int col=0;col<size;col++){
                System.out.print(tictacboard[row][col]);
+               if(col<size-1)System.out.print('|');
             }
             System.out.println();
-        }
+            }
     }
+    
     
     private void makeMove(){
         Scanner s = new Scanner(System.in);
+        System.out.println("this is "+currentplayer+" turn");
+        
         while (true){
             System.out.println("enter the row: ");
             int row=s.nextInt();
@@ -51,7 +54,6 @@ public class Tic_Tac_Toe {
 
             if (tictacboard[row][col]=='-'){
                 tictacboard[row][col]=currentplayer;
-                switchPlayer();
                 break;
             } else {
                 System.out.println("already that position is full");
@@ -68,12 +70,30 @@ public class Tic_Tac_Toe {
         } else {
             currentplayer=user1;
         }
-        System.out.println("current player changed to next user");
+        // System.out.println("current player changed to next user");
     }
 
-    private checkWin(){
+    private boolean checkWin(){
+        for(int row=0;row<size;row++){
+            if (tictacboard[row][0]==currentplayer && tictacboard[row][1]==currentplayer && tictacboard[row][2]==currentplayer){
+                return true;
+            }
+        }
+        for(int col=0;col<size;col++){
+            if (tictacboard[0][col]==currentplayer && tictacboard[1][col]==currentplayer && tictacboard[2][col]==currentplayer){
+                return true;
+            }
+        }
 
+        if (tictacboard[0][0]==currentplayer && tictacboard[1][1]==currentplayer && tictacboard[2][2]==currentplayer){
+            return true;
+        }
 
+        if (tictacboard[0][2]==currentplayer && tictacboard[1][1]==currentplayer && tictacboard[2][0]==currentplayer){
+            return true;
+        }
+        return false;
+    
     }
 
     private boolean checkTie(){
@@ -90,16 +110,27 @@ public class Tic_Tac_Toe {
     }
 
 
+    private void playgame(){
+        while (true) {
+            makeMove();
+            displayboard();
+            if (checkWin()){
+                System.out.println("player "+currentplayer+" wins!");
+                break;
+            } else if (checkTie()){
+                System.out.println("game is tie!");
+                break;
+
+            } else {
+                switchPlayer();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Tic_Tac_Toe tic=new Tic_Tac_Toe();
-        tic.displayboard();
-        tic.makeMove();
-        tic.switchPlayer();
-        tic.checkTie();
+        tic.playgame();
     
         }
-
-       
-
 }
 
